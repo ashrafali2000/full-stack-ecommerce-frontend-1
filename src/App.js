07ea49a2477f02a84517e5106746fe620b465fe4
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-16 10:18:08 +0500

    create routes in app page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./component/navbar";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Products from "./pages/products";
+import SignIn from "./pages/signIn";
+import SignUp from "./pages/signUp";
+import ShopingCart from "./pages/shopingCart";
+
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element = {<Navbar></Navbar>}>
+         <Route index element = {<Home></Home>}/>
+         <Route path="about" element = {<About></About>}/>
+         <Route path="contact" element = {<Contact></Contact>}/>
+         <Route path="products" element = {<Products></Products>}/>
+         <Route path="signin" element = {<SignIn></SignIn>}/>
+         <Route path="signup" element = {<SignUp></SignUp>}/>
+         <Route path="shopingcart" element = {<ShopingCart></ShopingCart>}/>
+
+        </Route>
+      </Routes>
     </div>
   );
 }
