77bee71cb86399d53e1db482c4d169e45e124e0f
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-16 10:17:34 +0500

    update shopig cart name

---
+++ src/pages/shopingCart/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Shoping_Cart from '../../component/shoping-cart'
+
+
+const ShopingCart = () => {
+  return (
+    <div>
+        <div>Shoping-Cart</div>
+         <Shoping_Cart></Shoping_Cart>
+    </div>
+  )
+}
+
+export default ShopingCart
