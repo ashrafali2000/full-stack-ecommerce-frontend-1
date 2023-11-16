4f7434028df0b31d4a51cfb2eb0c6672a0876a6a
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-16 10:48:01 +0500

    set logo

--- src/component/form/index.js
+++ src/component/form/index.js
@@ -1,11 +1,12 @@
+import logo from "../../images/stylo logo.png"
 export default function Form({signIn}) {
     return (
       <>
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              className="mx-auto h-24 w-auto"
+              src={logo}
               alt="Your Company"
             />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
