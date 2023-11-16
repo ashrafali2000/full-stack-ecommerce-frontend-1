61cad9e6ff26c5317bf802ca6f7757263174d045
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-16 10:10:14 +0500

    create signUp page

---
+++ src/pages/signUp/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from '../../component/form'
+
+const SignUp = () => {
+  return (
+    <div>
+        <div>SignUp</div>
+        <Form SignUp ></Form>
+
+    </div>
+  )
+}
+
+export default SignUp
