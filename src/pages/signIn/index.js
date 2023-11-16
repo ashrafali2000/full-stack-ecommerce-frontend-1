411da9767b42a981eebd76289bd8b2774a03f3d9
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-16 10:10:04 +0500

    create signIn page

---
+++ src/pages/signIn/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from '../../component/form'
+
+const SignIn = () => {
+  return (
+    <div>
+        <div>SignIn</div>
+        <Form signIn ></Form>
+
+    </div>
+  )
+}
+
+export default SignIn
