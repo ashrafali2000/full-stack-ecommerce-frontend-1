a4283d43e9e3c435892ddee16beee4f165c2ae63
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-18 11:54:41 +0500

    render all product in product file

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,21 +1,10 @@
-import React, { useState } from "react";
-import axios from "../../axios";
-import ProductCard from "../../component/ProductCard"
+import React from "react";
 const Home = () => {
-  const [products, setProducts] = useState([]);
-  axios
-    .get("/products")
-    .then((res) => setProducts(res.data))
-    .catch((err) => console.log(err));
+ 
   return (
     <div>
       <div className="pl-20 font-bold text-[20px] p-3 bg-orange-300">Home</div>
-      <div className="flex justify-center items-center flex-wrap gap-10 bg-orange-200 pt-8">
-        {products.map((p) => (
-          <ProductCard key={p._id} title = {p.title} description = {p.description} price={p.price} image= {p.images[0]}>
-          </ProductCard>
-        ))}
-      </div>
+      
     </div>
   );
 };
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import React ,{ useState } from 'react'
+import ProductCard from "../../component/ProductCard"
+import axios from "../../axios";
 const Products = () => {
-  return (
-    <div>Products</div>
+  const [products, setProducts] = useState([]);
+  axios
+    .get("/products")
+    .then((res) => setProducts(res.data))
+    .catch((err) => console.log(err));
+  return (<div>
+    <div className="pl-20 font-bold text-[20px] p-3 bg-orange-300">Home</div>
+    <div className="flex justify-center items-center flex-wrap gap-10 bg-orange-200 pt-8">
+        {products.map((p) => (
+          <ProductCard key={p._id} title = {p.title} description = {p.description} price={p.price} image= {p.images[0]}>
+          </ProductCard>
+        ))}
+      </div>
+  </div>
   )
 }
 
