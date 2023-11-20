8ca369ee6fc5098030205197ae284e4572c8df33
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-20 16:22:06 +0500

    add productContext provider

--- src/App.js
+++ src/App.js
@@ -7,22 +7,37 @@ import Products from "./pages/products";
 import SignIn from "./pages/signIn";
 import SignUp from "./pages/signUp";
 import ShopingCart from "./pages/shopingCart";
+import ProductContext from "./context/productContext";
+import { useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const getAllProducts = (data) => {
+    setProducts(data)
+}
   return (
     <div className="App">
+         <ProductContext.Provider value={
+          {
+            products: products, 
+            getAllProducts
+          }
+         }>
       <Routes>
         <Route path="/" element = {<Navbar></Navbar>}>
          <Route index element = {<Home></Home>}/>
          <Route path="about" element = {<About></About>}/>
          <Route path="contact" element = {<Contact></Contact>}/>
-         <Route path="products" element = {<Products></Products>}/>
+         <Route path="products" element = {<Products></Products>
+          }/>
          <Route path="signin" element = {<SignIn></SignIn>}/>
          <Route path="signup" element = {<SignUp></SignUp>}/>
          <Route path="shopingcart" element = {<ShopingCart></ShopingCart>}/>
 
         </Route>
       </Routes>
+          </ProductContext.Provider>
     </div>
   );
 }
