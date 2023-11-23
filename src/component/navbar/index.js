14fb89525875b98c4cd344c3e005e6df9cfe02d8
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-23 16:25:27 +0500

    anchor tag update into Link

--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -136,9 +136,9 @@ const getProductHandler = (data) => {
           </a>
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="/signin" className="text-sm font-semibold leading-6 text-gray-900">
+          <Link to="/signin" className="text-sm font-semibold leading-6 text-gray-900">
             Log in <span aria-hidden="true">&rarr;</span>
-          </a>
+          </Link>
         </div>
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
@@ -210,12 +210,12 @@ const getProductHandler = (data) => {
                 </a>
               </div>
               <div className="py-6">
-                <a
-                  href="/signin"
+                <Link
+                  to="/signin"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Log in
-                </a>
+                </Link>
               </div>
             </div>
           </div>
